7abd1e0e159d9fb0b9d8a0477a1413b70b81a4f5
25bme089-cell <25bme089@sot.pdpu.ac.in>
2026-02-10 16:56:42 +0530

    10-02-2026 work + added via upload

---
+++ Pract_5_LISTS.py
@@ -0,0 +1,8 @@
+strings = ["python", "list", "programming", "exam", "practice"]
+print("Original List:", strings)
+
+upper_list = []
+for s in strings:
+    upper_list.append(s.upper())
+
+print("Uppercase List:", upper_list)
