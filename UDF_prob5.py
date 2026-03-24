513fa17ef43b36f5cf91fe89e68e9f61b74a4d89
25bme089-cell <25bme089@sot.pdpu.ac.in>
2026-03-24 17:20:33 +0530

    modifying pangram program

--- UDF_prob5.py
+++ UDF_prob5.py
@@ -1,10 +1,11 @@
 def ispangram(z):
-    ch=("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    if z in ch:
+    z = z.lower()
+    
+    if set("abcdefghijklmnopqrstuvwxyz").issubset(set(z)):
         print("pangram")
     else:
         print("not pangram")
-    return 0
 
-str1=input("Enter string:")
-c=ispangram(str1)
+
+str1 = input("Enter string: ")
+ispangram(str1)
